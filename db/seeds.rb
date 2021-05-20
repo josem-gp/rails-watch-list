42ce595adcb51ac7485810e33abf44d238dde6dd
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-05-20 12:45:04 +0900

    Seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require "rest-client"
+
+puts "Deleting database..."
+
+Movie.delete_all
+
+puts "Calling API..."
+response = RestClient.get "http://tmdb.lewagon.com/movie/top_rated"
+repos = JSON.parse(response)
+# => repos is an `Array` of `Hashes`.
+repos['results'].each do |repo|
+  movie = Movie.new(title: repo['original_title'],
+                       overview: repo['overview'],
+                       poster_url: repo['poster_path'],
+                       rating: repo['vote_average'])
+  puts "Created movie: #{repo['original_title']}"
+  movie.save!
+end
+
+puts "Created all!"
