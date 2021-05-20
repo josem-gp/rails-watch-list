c825ee45ba5535ba3448f7a1e3ae94e87abd80ef
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-05-20 16:43:55 +0900

    done back-end

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,32 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @bookmark = Bookmark.new(strong_params)
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def strong_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
